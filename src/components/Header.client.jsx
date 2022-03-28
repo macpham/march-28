db1ee8203d33a2389bdc688e0212b71c6327759b
Mac Pham <1777724+macpham@users.noreply.github.com>
2022-03-28 16:06:36 -0400

    Update Header.client.jsx

--- src/components/Header.client.jsx
+++ src/components/Header.client.jsx
@@ -46,7 +46,7 @@ export default function Header({collections, storeName}) {
               className="font-black uppercase text-3xl tracking-widest"
               to="/"
             >
-              {storeName}
+              🎭 Staging! {storeName}
             </Link>
             <CartToggle
               handleClick={() => {
