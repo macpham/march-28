52ce7bf7f60734138937bfcec366d413ff40e154
Mac Pham <1777724+macpham@users.noreply.github.com>
2022-03-29 14:50:03 -0400

    Add some colour 🌈

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -31,7 +31,7 @@ export default function ProductCard({product}) {
           )}
         </div>
 
-        <span className="text-black font-semibold mb-0.5">✨ {product.title} ✨</span>
+        <span className="text-black font-semibold mb-0.5">🌈✨ {product.title} ✨🌈</span>
 
         {product.vendor && (
           <p className="text-gray-900 font-medium text-sm mb-0.5">
